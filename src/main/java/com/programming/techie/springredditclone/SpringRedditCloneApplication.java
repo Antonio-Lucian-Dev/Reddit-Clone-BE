f1bae0c787553b6c5d58b6fecb1d615193102db0
Antonio Lucian <web.antonio.popa@gmail.com>
2021-12-02 13:08:43 +0200

    Initial commit

--- src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
+++ src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
@@ -12,6 +12,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SpringRedditCloneApplication {
 
     public static void main(String[] args) {
+        String ENV_PORT = System.getenv().get("PORT");
+        String ENV_DYNO = System.getenv().get("DYNO");
+        if(ENV_PORT != null && ENV_DYNO != null) {
+            System.getProperties().put("server.port", ENV_PORT);
+        }
+
         SpringApplication.run(com.programming.techie.springredditclone.SpringRedditCloneApplication.class, args);
     }
 
